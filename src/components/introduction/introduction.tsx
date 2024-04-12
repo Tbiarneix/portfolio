d82fd7b3a5c0a3c3e9368ce257e3ddb8bcf0bada
Thomas <thomas.biarneix@gmail.com>
2024-04-12 19:20:32 +0200

    feat: add social media logos

--- src/components/introduction/introduction.tsx
+++ src/components/introduction/introduction.tsx
@@ -1,21 +1,40 @@
 import styles from "./introduction.module.css";
+
+import Image from "next/image";
 import Menu from "./menu/menu";
+
+import githubLogoWhite from "/src/assets/github-logo-blanc.png";
+import linkedinLogoWhite from "/src/assets/linkedin-logo-blanc.png";
 
 function Introduction() {
   return (
-    <header className={styles.introduction}>
+    <header id="header" className={styles.introduction}>
       <div className={styles.top}>
         <h1>Thomas Biarneix</h1>
         <h2>Frontend Developer</h2>
         <p>
-          Passionate about interfaces,user experience, and involved in the
+          Passionate about interfaces, user experience, and involved in the
           creation of accessible and eco-responsible applications.
         </p>
-       <Menu />
+        <Menu />
       </div>
       <div className={styles.social}>
-        <a href="">LinkedIn</a>
-        <a href="">GitHub</a>
+        <a href="https://www.linkedin.com/in/tbiarneix/" target="_blank">
+          <Image
+            src={linkedinLogoWhite}
+            alt="LinkedIn"
+            width={50}
+            height={50}
+          />
+        </a>
+        <a href="https://github.com/Tbiarneix" target="_blank">
+          <Image
+            src={githubLogoWhite}
+            alt="GitHub"
+            width={50}
+            height={50}
+          />
+        </a>
       </div>
     </header>
   );
