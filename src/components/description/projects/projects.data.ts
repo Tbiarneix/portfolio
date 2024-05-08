afd07f0c878caa8d6bc38004b669dd6285efbcc3
Thomas <thomas.biarneix@gmail.com>
2024-05-08 23:32:56 +0200

    wip: project feature

--- src/components/description/projects/projects.data.ts
+++ src/components/description/projects/projects.data.ts
@@ -24,4 +24,12 @@ export const projects: IProjects[] = [
     url: "https://pro.schoolmouv.fr/manuels/",
     image: "/assets/logo-schoolmouv.png",
   },
+  {
+    title: "Thomas Biarneix's Portfolio",
+    description:
+      "Ce portfolio est l'occasion de vous faire découvrir mon parcours mais aussi de mettre en pratique ce qui me passionne le plus: la création d'interface simple, intuitive et accessible. J'ai utilisé ici des technologies que j'affectionne particulièrement et j'en ai profité pour ajouter quelques fonctionnalités supplémetaires comme la gestion de l'internationalisation, un module d'accessibilité ou encore la gestion des thèmes sombres et lumineux.",
+    tags: ["Next.Js", "Typescript", "GitHub Actions", "CI/CD"],
+    url: "https://github.com/Tbiarneix/portfolio",
+    image: "/assets/logo-schoolmouv.png",
+  },
 ];
